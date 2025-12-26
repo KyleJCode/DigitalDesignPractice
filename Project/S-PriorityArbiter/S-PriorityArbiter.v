c947e1aec0515d72e914c712141184a554fad080
KyleJCode <kylestyle05@gmail.com>
2025-12-26 10:05:42 -0500

    Finished priority arbiter

--- Project/S-PriorityArbiter/S-PriorityArbiter.v
+++ Project/S-PriorityArbiter/S-PriorityArbiter.v
@@ -36,11 +36,38 @@ begin
         gnt <= 0;
         
         case(ties)
-        3'b011: 
-        3'b110: 
-        3'b101: 
-        3'b111: 
-        default: 
+        3'b011: // req 0/1 tie
+        begin
+            // see if this works too: gnt <= (state == 2'b00 || state == 2'b10) ? 3'b001 : 3'b010; 
+            if(state == 2'b00 || state == 2'b10)
+                gnt <= 3'b001;
+            else
+                gnt <= 3'b010;
+        end
+        3'b110: // req 1/2 tie
+        begin
+            if(state == 2'b00 || state == 2'b10)
+                gnt <= 3'b100;
+            else
+                gnt <= 3'b010;
+        end
+        3'b101: // req 0/2 tie
+        begin
+            if(state == 2'b00 || state == 2'b10)
+                gnt <= 3'b001;
+            else
+                gnt <= 3'b100;
+        end
+        3'b111: // req 2/1/0 tie
+        begin
+            if(state == 2'b00)
+                gnt <= 3'b001;
+            else if(state == 2'b01)
+                gnt <= 3'b010
+            else
+                gnt <= 3'b100;
+        end
+        default:  
         begin
             gnt[0] <= ties[0];
             gnt[1] <= ties[1];
@@ -55,8 +82,11 @@ begin
                 state <= 2'b01;
             else if(state == 2'b01)
                 state <= 2'b10;
+            else if(state == 2'b10)
+                state <= 2'b11;
             else
                 state <= 2'b00;  
+    
         end  
     end 
 end
